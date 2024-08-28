9f7bdd14fdfa87810c1912e4d5bcdf0b0af907d8
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-08-28 14:57:54 +0930

    add eslint and create a server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
-import fastify from 'fastify';
+import { app } from './app'
 
-const app = fastify();
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
